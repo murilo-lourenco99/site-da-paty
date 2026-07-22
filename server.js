9ba2dbff62d722387941012f014cdee75124e0ed
murilo-lourenco99 <muriloljesus22@gmail.com>
2026-07-22 17:19:43 -0300

    Commit

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORTA = 4000;
 
-// Serve todos os arquivos da pasta raiz (index, CSS, JS, Imagens)
 app.use(express.static(__dirname));
 
-// Rota para abrir o index automaticamente
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
